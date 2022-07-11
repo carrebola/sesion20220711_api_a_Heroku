17551cf8b3407239186412d41f153331b2f09c67
Mona Lisa <carrebola@fpllefia.com>
2022-07-11 09:49:53 +0200

    puerto2

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const nombre = process.env.NOMBRE || 'Anónimo'
 app.get('/' , (req,res)=>{
     res.status(200).json({
         error:false,
-        mensaje: 'Estás en la api de ' + nombre
+        mensaje: 'Estás en la api de ' + nombre ,
         puerto: process.env.PORT
     })
 })
